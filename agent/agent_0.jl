38d6fc26f8b1b917ddc5b9fa221299576f1e731e
albertpod <albert.podusenko@gmail.com>
2020-11-05 17:46:01 +0100

    Add goal-driven inference

--- agent/agent_0.jl
+++ agent/agent_0.jl
@@ -1,6 +1,9 @@
 using ForneyLab
-using ProgressMeter
 using LinearAlgebra
+
+βN(x) = x
+sumβN(x, y) = x + y
+sigmoid(x) = 1/(1+ exp(-(x)))
 
 function init_estimator()
     n_samples = 1
@@ -20,9 +23,6 @@ function init_estimator()
     β = Vector{Variable}(undef, n_samples)
 
     u_i_min = u_0
-    βN(x) = x
-    sumβN(x, y) = x + y
-    sigmoid(x) = 1/(1+ exp(-(x)))
 
     for i in 1:n_samples
 
@@ -41,14 +41,14 @@ function init_estimator()
         # Reset state for next step
         u_i_min = un[i]
     end
-    q = PosteriorFactorization(ω, ωn, [u_0; un], ids=[:EΩ :EΩN :EU])
-    algo = messagePassingAlgorithm(free_energy=true)
+    q = PosteriorFactorization(ω, ωn, [u_0; un], ids=[:Ω :ΩN :U])
+    algo = messagePassingAlgorithm(free_energy=true, id=:E)
     source_code = algorithmSourceCode(algo, free_energy=true)
     eval(Meta.parse(source_code))
 end
 
-function estimate()
-    data = Dict(:β => 0.9, :m_u_0 => 0.0, :w_u_0 => 1.0)
+function estimate(n_samples; prediction, target, vmp_iter)
+    data = Dict(:β => target, :m_u_0 => prediction[1], :w_u_0 => prediction[2])
     # Initial posterior factors
     marginals = Dict{Symbol, ProbabilityDistribution}()
     for t = 1:n_samples
@@ -59,45 +59,46 @@ function estimate()
         marginals[:ωn_*t] = vague(GaussianMeanPrecision)
         marginals[:ωsample_*t] = vague(GaussianMeanPrecision)
     end
-
-    @showprogress for i in 1:10
+    fe = []
+    for i in 1:vmp_iter
         stepEU!(data, marginals)
         stepEΩ!(data, marginals)
         stepEΩN!(data, marginals)
+        push!(fe, freeEnergyE(data, marginals))
     end
+    return marginals, fe
 end
 
 function init_actor()
-    n_samples = 1
     fg = FactorGraph()
-    ω̂ = 0.9
     # State prior
     @RV u_0 ~ GaussianMeanPrecision(placeholder(:m_u_0), placeholder(:w_u_0))
-    @RV u ~ GaussianMeanPrecision(u_0, 100.0)
+    @RV u ~ GaussianMeanPrecision(u_0, 1.0)
     @RV usum = u + placeholder(:ω̂)
     @RV un ~ GaussianMeanPrecision(usum, 100.0)
-    sigmoid(x) = 1/(1+ exp(-x))
     @RV p ~ Nonlinear{Sampling}(un, g=sigmoid)
     @RV r ~ Bernoulli(p)
 
     # Data placeholder
     placeholder(r, :r)
 
-    q = PosteriorFactorization([u_0; u], un, ids=[:AU :AUN])
-    algo = messagePassingAlgorithm(free_energy=true)
+    q = PosteriorFactorization([u_0; u], un, ids=[:U :UN])
+    algo = messagePassingAlgorithm(free_energy=true, id=:A)
     source_code = algorithmSourceCode(algo, free_energy=true)
     eval(Meta.parse(source_code))
 end
 
-function act(action)
+function act(action; feedback, prior, vmp_iter)
     marginals = Dict{Symbol, ProbabilityDistribution}(:un => vague(GaussianMeanPrecision))
-    data = Dict(:ω̂ => action, :r=>0.0, :m_u_0 => 0.0, :w_u_0 => 0.1)
+    data = Dict(:ω̂ => action, :r=>0.0, :m_u_0 => prior[1], :w_u_0 => prior[2])
     messages_1 = Array{Message}(undef, 6)
     messages_2 = Array{Message}(undef, 4)
     fe = []
-    @showprogress for i in 1:10
+    for i in 1:vmp_iter
         stepAU!(data, marginals, messages_1)
         stepAUN!(data, marginals, messages_2)
-        push!(fe, freeEnergy(data, marginals))
+        push!(fe, freeEnergyA(data, marginals))
     end
+    
+    return marginals, fe
 end
