6240bec73b45c297035c302f9c2220b21a339752
albertpod <albert.podusenko@gmail.com>
2021-09-28 16:06:35 +0200

    Major restructure

--- helpers/ar_ssm.jl
+++
@@ -1,9 +0,0 @@
-function ar_ssm(series, order)
-    inputs = [reverse!(series[1:order])]
-    outputs = [series[order + 1]]
-    for x in series[order+2:end]
-        push!(inputs, vcat(outputs[end], inputs[end])[1:end-1])
-        push!(outputs, x)
-    end
-    return inputs, outputs
-end
--- src/environment/environment.jl
+++ src/environment/environment.jl
@@ -1,17 +1,9 @@
-module AIDAenv
 # models
 export ar_model, coupled_model, gaussian_model, lar_model, lar_model_ex
 export ar_inference, coupled_inference, inference_gaussian, lar_inference, lar_inference_ex
 export lar_batch_learning, batch_coupled_learning
 
-using Rocket
-using ReactiveMP
-using GraphPPL
-import ProgressMeter
-
 include("ar.jl")
 include("coupled_ar.jl")
 include("gaussian.jl")
 include("lar.jl")
-
-end
