1fdc42b16154c321e62ecd36bd6812f556fb934c
MagnusKoudahl <magnuskoudahl@gmail.com>
2021-11-02 14:19:35 +0100

    add updated pref learning example with Barts user response function

--- src/environment/user.jl
+++ src/environment/user.jl
@@ -24,3 +24,29 @@ function generate_user_response(w::AbstractVector; μ=[0.8, 0.2], a=1, b=1, c=25
     end
 
 end
+
+# Dispatch on tuples to make plotting the EFE landscape easier
+function generate_user_response(w::Tuple; μ=[0.8, 0.2], a=1, b=1, c=25, d=-0.4, binary=true)
+
+    @assert length(w) == 2 "The user preferences are currently only defined for 2-dimensional gains."
+
+    f(x) = -sqrt((x[1]-μ[1])^(2)/a + (x[2]-μ[2])^(2)/b)
+
+    z = zeros(10,10)
+    for kx = 1:10
+        for ky = 1:10
+            z[kx, ky] = f([kx/10, ky/10])
+        end
+    end
+
+    maxz, minz = maximum(z), minimum(z)
+
+    p = 1 ./ (1 + exp.(-c*((f(w)-minz)/(maxz-minz)-0.5+d)))
+
+    if binary
+        return 1.0*rand(Bernoulli(p))
+    else
+        return p
+    end
+
+end
