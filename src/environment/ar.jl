1703de32cd907b9fd4f28c8f12ec279587e60910
albertpod <albert.podusenko@gmail.com>
2021-11-05 15:05:22 +0100

    Update demos

--- src/environment/ar.jl
+++ src/environment/ar.jl
@@ -30,7 +30,7 @@ function ar_inference(inputs, outputs, order, niter, aγ=1.0, bγ=1.0)
 
     setmarginal!(γ, GammaShapeRate(aγ, bγ))
 
-    for i in 1:niter
+    ProgressMeter.@showprogress for i in 1:niter
         update!(x, inputs)
         update!(y, outputs)
     end
