f3883a51a449306cec5195a5053859bc433f6f38
albertpod <albert.podusenko@gmail.com>
2021-11-05 16:35:51 +0100

    Updates

--- src/environment/ar.jl
+++ src/environment/ar.jl
@@ -18,7 +18,7 @@ end
 # AR inference
 function ar_inference(inputs, outputs, order, niter, aγ=1.0, bγ=1.0)
     n = length(outputs)
-    model, (x, y, θ, γ) = ar_model(n, order, aγ, bγ, options = (limit_stack_depth = 500, ))
+    model, (x, y, θ, γ) = ar_model(n, order, aγ, bγ, options = (limit_stack_depth = 100, ))
 
     γ_buffer = nothing
     θ_buffer = nothing
