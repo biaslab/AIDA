b02ec598f22f543acf2bf57f6d9030d6b5343ec1
MagnusKoudahl <magnuskoudahl@gmail.com>
2021-12-13 09:38:32 +0100

    update with probit likelihood

--- src/agent/agent.jl
+++ src/agent/agent.jl
@@ -1,4 +1,4 @@
-export get_new_proposal, optimize_hyperparams, get_new_decomp
+export get_new_proposal, optimize_hyperparams, get_new_decomp, get_new_pointvalues
 
 include("plot_utils.jl")
 include("utils.jl")
