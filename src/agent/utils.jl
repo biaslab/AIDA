6ee5b7df52ce8760f6b7bd1927e7532c6c4c0c64
albertpod <albert.podusenko@gmail.com>
2021-12-14 15:57:25 +0100

    Updates

--- src/agent/utils.jl
+++ src/agent/utils.jl
@@ -15,12 +15,10 @@ H(μ, σ) = (C / √(σ + C^2)) * exp(-0.5 * (μ^2 / (σ + C^2)))
 
 # BALD objective. Sometimes we need to dispatch on tuples
 BALD(μ, σ) = h(ϕ(μ, σ)) - H(μ, σ)
-#BALD((μ, σ)) = h(ϕ(μ, σ)) - H(μ, σ)
 BALD(θ) = BALD(θ[1][1],θ[2][1])
 
 # Cross entropy
 CE(μ,Σ) = KL(ϕ(μ, Σ), 1 - eps()) + h(ϕ(μ, Σ))
-#CE((μ,Σ)) = KL(ϕ(μ, Σ), 1 - eps()) + h(ϕ(μ, Σ))
 CE(θ) = CE(θ[1][1],θ[2][1])
 
 # KL between Bernoullis. Goal prior has param set to 1 - eps(). eps for stability
@@ -52,26 +50,6 @@ function predict(x1, grid_element, y1,Σ_11, σ, l)
     return μ_pred, Σ_pred
 end
 
-# Newton optimization to find the mean of the latent function
-#function find_μ(Σ, y) # W is of the proposed mean. Because we can get the precision as the Hessian of the log likelihood
-#    iter = 10
-#    # Make outputs -1/1 instead of 0/1
-#    y_scaled = (y .- 0.5) * 2.0
-#    μ_test = zeros(size(y, 1))
-#
-#    for i = 1:iter # Need to make a check for convergence here
-#	logp,∇_logp,∇∇_logp = ∇_log_prob(y_scaled,μ_test)
-#	W_ = Diagonal( -∇∇_logp)
-#	#μ_test = Σ * a
-#	μ_test = inv(Σ + inv(W_)) * (W_*μ_test + ∇_logp)
-#	# Objective for convergence
-##	obj_new = -0.5*a'*(μ_test) + sum(logp)
-#			       #.- log.(diag(L.L)))
-#        #μ_test = Σ * inv(I(size(y, 1)) + W(μ_test) * Σ) * (y_scaled - sigmoid.(μ_test) + W(μ_test) * μ_test)
-#    end
-#    μ_test
-#end
-
 # Objective function for finding the mean of the latent function
 function objective(a,y_scaled,Σ_11)
     μ = Σ_11'* a
@@ -102,9 +80,8 @@ function find_μ(Σ, y)
 	b = W_*μ_test + ∇_logp
 	Δ_a = b - sqrt(W_) * (L.U \ ( L.L \ ( sqrt(W_) * Σ * b))) - a
 
-        # Linesearch to determine the step size
+    # Linesearch to determine the step size
 	γ_star = optimize(γ -> objective(a + γ*Δ_a,y_scaled,Σ), 0.0, 2.0, abs_tol = 1e-4, method = Optim.Brent()).minimizer
-	#γ_star = optimal_stepsize.minimizer
 
 	# update the mean
 	a = a + γ_star * Δ_a
@@ -155,10 +132,6 @@ function log_evidence(x1, y1, θ)
     -ll
 end
 
-#function sigmoid(x)
-#    1 / (1 + exp(-x))
-#end
-
 # Optimize hyperparams of SE kernel
 function optimize_hyperparams(x1, y1, θ)
     # We bound params to ensure the optimization is stable. When the agent only has negative responses
@@ -169,21 +142,7 @@ function optimize_hyperparams(x1, y1, θ)
     σ,l = params.minimizer
 end
 
-
-
-#function instrumental(x1, xc, y1, σ, l)
-#    x2 = collect(xc)
-#    μ_pred, Σ_pred = predict(x1, x2, y1, σ, l)
-#    KL(ϕ(μ_pred[1], Σ_pred[1]), 1 - eps()) + h(ϕ(μ_pred[1], Σ_pred[1]))
-#end
-#
-#function epistemic(x1, xc, y1, σ, l)
-#    x2 = collect(xc)
-#    μ_pred, Σ_pred = predict(x1, x2, y1, σ, l)
-#    -BALD(μ_pred[1], Σ_pred[1])
-#end
-#
-function get_new_decomp(grid, x1, y1, current, σ, l)
+function get_new_decomp(grid, x1, y1, σ, l)
     n = size(x1,2)
     Σ_11 = se_kernel(x1, x1, σ, l) + I(n) * 1e-6
     # Compute the EFE grid
@@ -191,40 +150,16 @@ function get_new_decomp(grid, x1, y1, current, σ, l)
     epi_grid = -BALD.(pred_grid)
     inst_grid = CE.(pred_grid)
 
-    value_grid = epi_grid + inst_grid
-    # Ensure that we propose a new trial and not the same one twice in a row
-    #value_grid[collect(grid).==[(current[1], current[2])]] .= Inf
-
-    # Find the minimum and try it out
-    #idx = argmin(value_grid)
-    #x2 = collect(grid)[idx]
-    #x2, epi_grid, inst_grid, value_grid, idx
-    epi_grid,inst_grid
+    epi_grid, inst_grid
 end
-
-#function get_new_pointvalues(grid, x1, y1, current, σ, l)
-#    value_grid = choose_point.(Ref(x1), grid, Ref(y1), σ, l)
-#    # Ensure that we propose a new trial and not the same one twice in a row
-#    value_grid[collect(grid).==[(current[1], current[2])]] .= Inf
-#
-#    # Find the minimum and try it out
-#    idx = argmin(value_grid)
-#
-#    # Return epistemic/instrumental values at optimum
-#    epi = epistemic(x1, collect(grid)[idx], y1, σ, l)
-#    inst = instrumental(x1, collect(grid)[idx], y1, σ, l)
-#
-#    x2 = collect(grid)[idx]
-#    x2, epi, inst
-#end
-
 
 
 # Grid search over EFE values with inhibition of return, inspired by eye movements
 function get_new_proposal(grid, x1, y1, current, σ, l)
     # Compute the EFE grid
     Σ_11 = se_kernel(x1, x1, σ, l) + I(n) * 1e-6
-    value_grid = choose_point.(Ref(x1), grid, Ref(y1),Ref(Σ_11), σ, l)
+    epi_grid, inst_grid = get_new_decomp(grid, x1, y1, σ, l)
+    value_grid = epi_grid + inst_grid
     # Ensure that we propose a new trial and not the same one twice in a row
     value_grid[collect(grid).==[(current[1], current[2])]] .= Inf
 
